9062d34b177255612e4d1f2d3f327d98de913209
hezhi <hezhi.woo@gmail.com>
2020-11-28 22:40:55 +0800

    Initail DJActivity.java

---
+++ DJCommon/src/main/java/com/dj/djcommon/DJActivity.java
@@ -0,0 +1,14 @@
+package com.dj.djcommon;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DJActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
